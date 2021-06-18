d8b001d1f9d038eade719d383bdc6d5850956dd7
Rascal_Two <therealrascaltwo@gmail.com>
2021-06-18 11:37:09 -0400

    Rename DB_CONNECTION_URI to SEQUELIZE_OPTIONS

--- src/constants.ts
+++ src/constants.ts
@@ -7,6 +7,6 @@ dotenv.config();
 export const PORT = process.env['PORT'] || 5000;
 export const STATIC_DIRECTORY = path.join(__dirname, process.env['STATIC_DIRECTORY'] || './static');
 export const SEQUELIZE_OPTIONS = JSON.parse(
-  process.env['DB_CONNECTION_URI'] || JSON.stringify({ dialect: 'sqlite', storage: './database.db' }),
+  process.env['SEQUELIZE_OPTIONS'] || JSON.stringify({ dialect: 'sqlite', storage: './database.db' }),
 ) as Options;
 export const CORS_ORIGIN = process.env['CORS_ORIGIN'] || false;
